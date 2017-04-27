4c23ae4fa7daa2761cf495675be3bce6d3dc2327
Ogorelkov Sergey <sergey.ogorelkov@slotconstructor.ru>
2017-04-27 16:31:32 +0700

    Continue implementing server funcs

--- TcpLibrary/ClientSocket.cs
+++ TcpLibrary/ClientSocket.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace TcpLibrary
 {
@@ -7,6 +8,20 @@ namespace TcpLibrary
     {
         readonly TcpClient _client;
         public Guid Id { get; }
+        int _bufferSize;
+        public int BufferSize
+        {
+            get { return _bufferSize; }
+            set
+            {
+                if (value < 2)
+                    throw new ArgumentOutOfRangeException("Buffer size is too small");
+                if (value > (2 ^ 32))
+                    throw new ArgumentOutOfRangeException("Buffer size is too large");
+                
+                _bufferSize = value;
+            }           
+        }
 
         public ClientSocket(TcpClient client)
         {
@@ -17,6 +32,14 @@ namespace TcpLibrary
         internal NetworkStream GetStream()
         {
             return _client.GetStream();
+        }
+
+        public async Task SendAsync(byte[] data)
+        {
+            using (var networkStream = _client.GetStream())
+            {
+                await networkStream.WriteAsync(data, 0, data.Length);
+            }
         }
 
         public void Disconnect()
